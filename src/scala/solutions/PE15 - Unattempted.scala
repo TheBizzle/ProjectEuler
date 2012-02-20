8de3cf354e351d159cf9e7765dd9542b9b26bee6
TheBizzle <jason.bertsche@gmail.com>
2012-02-19 19:33:32 -0600

    I don't want to solve problem 15

---
+++ src/scala/solutions/PE15 - Unattempted.scala
@@ -0,0 +1,16 @@
+package scala.solutions
+
+import structures.core.PEApp
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Jason
+ * Date: 2/19/12
+ * Time: 7:32 PM
+ */
+
+// Uninterested!
+object PE15 extends PEApp {
+    run()
+    def myAnswer = "Who cares?"
+}

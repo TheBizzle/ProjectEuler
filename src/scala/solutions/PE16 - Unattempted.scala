b99eecf18c1d58f44a1cd4598308340261da9f8a
TheBizzle <jason.bertsche@gmail.com>
2012-02-19 19:39:35 -0600

    Ignoring another problem...

---
+++ src/scala/solutions/PE16 - Unattempted.scala
@@ -0,0 +1,16 @@
+package scala.solutions
+
+import structures.core.PEApp
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Jason
+ * Date: 2/19/12
+ * Time: 7:38 PM
+ */
+
+// More uninteresting rubbish
+object PE16 extends PEApp {
+    run()
+    def myAnswer = "I don't feel like jumping through hoops for the sake of jumping through hoops"
+}

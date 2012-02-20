f4e6d6265269a98709a7c70de99624360c65e8c7
TheBizzle <jason.bertsche@gmail.com>
2012-02-19 18:12:08 -0600

    Problem 13 SKIPPED (does not strike me as interesting or worth my time)

---
+++ src/scala/solutions/PE13 - Unattempted.scala
@@ -0,0 +1,16 @@
+package scala.solutions
+
+import structures.core.PEApp
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Jason
+ * Date: 2/19/12
+ * Time: 6:10 PM
+ */
+
+// Skipped!
+object PE13 extends PEApp {
+    run()
+    def myAnswer = "Bugger off!"
+}
